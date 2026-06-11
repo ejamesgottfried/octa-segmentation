9596ee2de18b7ffdc786f6f4fb677443c1e3cfcb
ejamesgottfried <edward_gottfried@brown.edu>
2026-06-11 19:56:01 -0400

    fix

--- src/model.py
+++ src/model.py
@@ -8,13 +8,13 @@ class DoubleConv(nn.Module):
         super().__init__()
         self.double_conv = nn.Sequential(
             # first conv: batchnorm and ReLU
-                nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False)
-                nn.BatchNorm2d(out_channels)
-                nn.ReLU(inplace=True)
+                nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU(inplace=True),
                 # second conv:  batchnorm and ReLU
-                nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False)
-                nn.BatchNorm2d(out_channels)
-                nn.ReLU(inplace=True)
+                nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU(inplace=True),
             )
     
     # one forward block
